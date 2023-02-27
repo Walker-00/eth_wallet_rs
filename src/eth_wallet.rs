b36e6099f0d4a43d67957c98043c5009b86c9106
Linus Walker <w000alker@gmail.com>
2023-02-27 13:18:06 +0630

    checking pub addr generation

--- src/eth_wallet.rs
+++ src/eth_wallet.rs
@@ -2,9 +2,19 @@ use secp256k1::{
     rand::{rngs::StdRng, SeedableRng},
     PublicKey, Secp256k1, SecretKey,
 };
+use web3::{signing::keccak256, types::Address};
 
 pub fn gen_key() -> (SecretKey, PublicKey) {
     let secp = Secp256k1::new();
-    let mut rng = StdRng::seed_from_u64(1121);
+    let mut rng = StdRng::seed_from_u64(111);
     secp.generate_keypair(&mut rng)
+}
+
+pub fn pub_key_addr(pub_key: &PublicKey) -> Address {
+    let pub_key = pub_key.serialize_uncompressed();
+
+    debug_assert_eq!(pub_key[0], 0x04);
+    let hash = keccak256(&pub_key[1..]);
+
+    Address::from_slice(&hash[12..])
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 mod eth_wallet;
 use eth_wallet::gen_key;
 
+use crate::eth_wallet::pub_key_addr;
+
 fn main() {
     let (sec_key, pub_key) = gen_key();
 
     println!(
-        "sec_key:  {}\npub_key:    {}",
-        sec_key.display_secret(),
+        "sec_key:    {:?}\npub_key:    {}",
+        sec_key,
         pub_key.to_string()
     );
+
+    let pub_addr = pub_key_addr(&pub_key);
+
+    print!("addr:    {:?}", pub_addr);
 }

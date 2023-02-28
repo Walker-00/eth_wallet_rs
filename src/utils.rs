6a755a1b863dc3ec03cd7ee61f18a994a9672ea7
Linus Walker <w000alker@gmail.com>
2023-02-28 18:32:16 +0630

    added web3 connection

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use anyhow::Result;
+use web3::types::U256;
 
 pub fn gen_systime() -> Result<u64> {
     let dur = SystemTime::now().duration_since(UNIX_EPOCH)?;
